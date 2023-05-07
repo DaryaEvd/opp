d41f505110ff839a5d482d634676ad2239378b5a
Darya Evdokimova <evddaryaevg@gmail.com>
2023-05-08 00:11:55 +0700

    changed matrix to struct matrix

--- pract5/code/letsgoo.cpp
+++ pract5/code/letsgoo.cpp
@@ -1,45 +1,75 @@
 #include <iostream>
 #include <fstream>
 
-void initMatrix(int *matrix, int rowsAmount, int columnsAmount) {
-  for(int i = 0; i < rowsAmount; i++) {
-    for(int j = 0; j < columnsAmount; j++) {
-      matrix[i * columnsAmount + j] = i;
+struct MyMatrix {
+  double *data = nullptr;
+  size_t colmns;
+  size_t rows;
+
+  MyMatrix(size_t rows, size_t colmns) {
+    data = new double[colmns * rows];
+    this->rows = rows;
+    this->colmns = colmns;
+  }
+};
+
+void initMatrix(MyMatrix matrix) {
+  for (size_t i = 0; i < matrix.rows; ++i) {
+    for (size_t j = 0; j < matrix.colmns; ++j) {
+      matrix.data[i * matrix.colmns + j] = i;
     }
   }
 }
 
+void freeMatrix(MyMatrix matrix) { delete[] matrix.data; }
+
+void printMatrixToFile(MyMatrix matrix, std::fstream &file) {
+  for (size_t i = 0; i < matrix.rows; ++i) {
+    for (size_t j = 0; j < matrix.colmns; ++j) {
+      file << matrix.data[i * matrix.colmns + j] << " ";
+    }
+    file << "\n" ;
+  }
+}
+
 int main(int argc, char **argv) {
-  if(argc != 3) {
-    printf("Bad amount of arguments!\n" \
-    "Enter rows amount, then enter columns amount.\n");
+  if (argc != 4) {
+    std::cout
+        << "Bad amount of arguments!\n"
+        << "Enter rows amount, then enter columns amount.\n"
+        << "Also enter mode of work 'p' - pattern or 'r' - random."
+        << std::endl;
     return 0;
   }
- 
+
   const int rowsAmount = atoi(argv[1]);
   const int columnsAmount = atoi(argv[2]);
+  const char modeToWork = argv[3][0];
 
-  int *matrix = new int[rowsAmount * columnsAmount];
-
-  initMatrix(matrix, rowsAmount, columnsAmount);
-
-  std::ofstream inputFile;
+  std::fstream inputFile;
   inputFile.open("start.txt");
-  if(!inputFile) {
-    printf("Can't open input file\n");
+  if (!inputFile) {
+    std::cout << "Can't open input file\n";
     return 0;
   }
 
-  for(size_t row = 0; row < rowsAmount; row++) {
-    for(size_t column = 0; column < columnsAmount; column++) {
-      inputFile << matrix[row * columnsAmount + column] << " ";
-    }
-    inputFile << "\n";
+  MyMatrix matrixStart = MyMatrix(rowsAmount, columnsAmount);
+
+  if (modeToWork == 'r') {
+    std::cout << "You're in random mode" << std::endl;
+
+    initMatrix(matrixStart);
+    printMatrixToFile(matrixStart, inputFile);
+
   }
 
+  if (modeToWork == 'p') {
+
+  }
   inputFile.close();
 
-  delete [] matrix;
+
+  freeMatrix(matrixStart);
 
   return 0;
 }
